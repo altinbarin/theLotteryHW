c5b99d35b8fe4287c922a8f70930302b8220ade7
altinbarin <furkanaltinbarin@gmail.com>
2023-07-20 23:27:46 +0300

    SayisalLoto

--- SayısalLotoOdev/Program.cs
+++ SayısalLotoOdev/Program.cs
@@ -1,4 +1,6 @@
-﻿internal class Program
+﻿using System.Linq;
+
+internal class Program
 {
 
     private static void Main(string[] args)
@@ -6,8 +8,9 @@
         //Ekrandan girilen kolon sayısı kadar 6 adet sayı üreten ve ürettiği sayıları sıralayarak ekrana yazıdırın.Rastgele sayı üretilirken aynı rakam denk gelirse tekrar sayı üretmeli.1-91 arasında rakamlardan oluşacak kolonlar.
 
         Random rnd = new Random();
-        int kolonSayisi, rastgeleSayi;
+        int kolonSayisi, rastgeleSayi, rastgeleSayi1;
         int[] sayilar = new int[6];
+        int[] kazananSayilar = new int[6];
         Console.Write("Kaç kolon oynamak istersiniz? ");
         kolonSayisi = Convert.ToInt32(Console.ReadLine());
 
@@ -30,8 +33,27 @@
             }
             Console.WriteLine("\n");
         }
-        
-        
+
+
+        for (int k = 0; k < kazananSayilar.Length; k++)
+        {
+            rastgeleSayi1 = rnd.Next(1,91);
+            if(!kazananSayilar.Contains(rastgeleSayi1))
+            {
+                kazananSayilar[k] = rastgeleSayi1;
+            }
+            else
+            {
+                k--;
+            }
+        }
+        Array.Sort(kazananSayilar);
+        Console.Write("Kazanan Sayılar: ");
+        foreach (int item in kazananSayilar)
+        {
+            Console.Write(item+" ");
+        }
+
 
 
     }
